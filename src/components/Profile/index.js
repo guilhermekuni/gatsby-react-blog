048f5a862bb6fbdcfd40e66b5a3539b626d044e0
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-25 20:08:56 -0300

    implements query using useStaticQuery hook

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
-import { StaticQuery, graphql } from 'gatsby';
+import { useStaticQuery, graphql } from 'gatsby';
 
-const Profile = () => (
-  <StaticQuery
-    query={graphql`
-    query MyQuery {
-      site {
-        siteMetadata {
-          title
-          position
-          description
-          author
-        }
+const Profile = () => {
+  const { site: { siteMetadata } } = useStaticQuery(graphql`
+  query MyQuery {
+    site {
+      siteMetadata {
+        title
+        position
+        description
+        author
       }
-    }    
-    `}
-    render={({ site: { siteMetadata } }) => (
-      <div className="Profile-wrapper">
-        <h1>{siteMetadata.title}</h1>
-        <h2>{siteMetadata.position}</h2>
-        <p>{siteMetadata.description}</p>
-      </div>
-    )}
-  />
-);
+    }
+  }
+  `);
+
+  return (
+    <div className="Profile-wrapper">
+      <h1>{siteMetadata.title}</h1>
+      <h2>{siteMetadata.position}</h2>
+      <p>{siteMetadata.description}</p>
+    </div>
+  )
+}
 
 export default Profile;
