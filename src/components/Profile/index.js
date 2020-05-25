11c78debafc28b68b7cdb733f65038fddbc099f4
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-25 19:29:26 -0300

    create initial profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Guilherme Kuniyshi</h1>
+    <h2>Desenvolvedor Front-end</h2>
+    <p>Descrição</p>
+  </div>
+);
+
+export default Profile;
