14aae0e23c3ce74a16ec66ac55ff810dee7a9168
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-25 20:02:47 -0300

    implements StaticQuery

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 const Profile = () => (
-  <div className="Profile-wrapper">
-    <h1>Guilherme Kuniyshi</h1>
-    <h2>Desenvolvedor Front-end</h2>
-    <p>Descrição</p>
-  </div>
+  <StaticQuery
+    query={graphql`
+    query MyQuery {
+      site {
+        siteMetadata {
+          title
+          position
+          description
+          author
+        }
+      }
+    }    
+    `}
+    render={({ site: { siteMetadata } }) => (
+      <div className="Profile-wrapper">
+        <h1>{siteMetadata.title}</h1>
+        <h2>{siteMetadata.position}</h2>
+        <p>{siteMetadata.description}</p>
+      </div>
+    )}
+  />
 );
 
 export default Profile;
