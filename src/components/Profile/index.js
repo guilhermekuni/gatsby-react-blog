0f2810a5831371ca52bc91644c7f7546203a79e1
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-26 21:29:44 -0300

    create Avatar component with gatsby-image

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 
+import Avatar from '../Avatar';
+
 const Profile = () => {
   const { site: { siteMetadata } } = useStaticQuery(graphql`
   query MyQuery {
@@ -17,6 +19,7 @@ const Profile = () => {
 
   return (
     <div className="Profile-wrapper">
+      <Avatar />
       <h1>{siteMetadata.title}</h1>
       <h2>{siteMetadata.position}</h2>
       <p>{siteMetadata.description}</p>
