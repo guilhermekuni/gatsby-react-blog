c432e6f91ca815f081f19bb66102c0a636ac8bd9
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-27 20:50:08 -0300

    implements SocialLinks component

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Profile from '../Profile';
+import SocialLinks from '../SocialLinks';
 
 import * as S from './styles';
 
 const SideBar = () => (
   <S.StyledWrapper>
     <Profile />
+    <SocialLinks />
   </S.StyledWrapper>
 )
 
