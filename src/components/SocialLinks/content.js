370a71bd963548e8050bb5a8ae5f40ed191cd4d7
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-05-27 20:30:34 -0300

    create Icon and content file

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,24 @@
+const links = [
+  {
+    label: 'Github',
+    url: 'https://github.com/guilhermekuni',
+  },
+  {
+    label: 'Twitter',
+    url: 'https://twitter.com/guilhermekuni',
+  },
+  {
+    label: 'Youtube',
+    url: 'https://youtube.com/guilhermekuni',
+  },
+  {
+    label: 'Instagram',
+    url: 'https://instagram.com/guilhermekuni',
+  },
+  {
+    label: 'Unsplash',
+    url: 'https://unsplash.com/guilhermekuni',
+  },
+];
+
+export default links;
