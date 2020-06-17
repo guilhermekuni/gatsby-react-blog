aab74f23108c9527a9413771fc500dd73600207c
Guilherme Kuniyoshi <guilhermekuni@gmail.com>
2020-06-17 19:29:22 -0300

    implements Pagination component

--- src/templates/blog-list.js
+++ src/templates/blog-list.js
@@ -4,9 +4,17 @@ import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import SEO from '../components/seo';
 import PostItem from '../components/PostItem';
+import Pagination from '../components/Pagination';
 
 const BlogList = props => {
   const postList = props.data.allMarkdownRemark.edges;
+
+  const { currentPage, numPages } = props.pageContext
+
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? '/' : `/page/${currentPage - 1}`
+  const nextPage = `/page/${currentPage + 1}`
 
   return (
     <Layout>
@@ -31,6 +39,15 @@ const BlogList = props => {
             />
           ))
       }
+
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        currentPage={currentPage}
+        numPages={numPages}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
     </Layout>
   )
 };
